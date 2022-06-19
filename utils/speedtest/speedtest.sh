80307cdb646b951a4b45a75cc31321c30e8b9c8c
reny311 <99379394+reny311@users.noreply.github.com>
2022-06-19 10:46:47 +0800

    Update speedtest.sh

--- utils/speedtest/speedtest.sh
+++ utils/speedtest/speedtest.sh
@@ -3,9 +3,9 @@ wget -O clash.gz https://github.com/Dreamacro/clash/releases/download/v1.10.6/cl
 gunzip clash.gz
 wget -O lite.gz https://github.com/alanbobs999/LiteSpeedTest/releases/download/v0.9.1/lite-linux-amd64-v0.9.1.gz
 gunzip lite.gz
-wget -O clash_config.yml https://raw.githubusercontent.com/reny311/TopFreeProxies/master/utils/speedtest/clash_config.yml
-wget -O proxychains.conf https://raw.githubusercontent.com/reny311/TopFreeProxies/master/utils/speedtest/proxychains.conf
-wget -O lite_config.json https://raw.githubusercontent.com/reny311/TopFreeProxies/master/utils/speedtest/lite_config.json
+wget -O clash_config.yml https://raw.githubusercontent.com/reny311/proxyTop/master/utils/speedtest/clash_config.yml
+wget -O proxychains.conf https://raw.githubusercontent.com/reny311/proxyTop/master/utils/speedtest/proxychains.conf
+wget -O lite_config.json https://raw.githubusercontent.com/reny311/proxyTop/master/utils/speedtest/lite_config.json
 #初始化 Clash
 chmod +x ./clash && ./clash &
 #安装并配置 proxychains
